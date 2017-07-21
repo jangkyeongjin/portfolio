00b1730713f30961df3f360c8f6783a6a19b8dad
jangkyeongjin <jkj318085@gmail.com>
2017-07-21 18:30:36 +0900

    0721

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,19 @@
 $(function(){
-   $('.main .navi .menu').hover(
-     function(){
-       $('.slide_menu .sl_m').stop().slideDown();
-     },
-     function(){
-       $('.slide_menu .sl_m').stop().slideUp();
-     })
+
+   $('.main .nav .menu ul > li').hover(function(){
+     console.log('ㅇ');
+     $('.slide_menu').stop().slideDown();
+
+   },function(){
+     $('.slide_menu').stop().slideUp();
+   })
+
 })
+//
+// function(){
+//   console.log('ㅇㅇ');
+//   $('.slide_menu .sl_m').stop().slideDown();
+// },
+// function(){
+//   $('.slide_menu .sl_m').stop().slideUp();
+// })
